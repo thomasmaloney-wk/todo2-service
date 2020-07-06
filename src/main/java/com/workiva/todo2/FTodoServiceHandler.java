c00327306f1cb8a5617c574d61b7904313e5f3a2
thomasmaloney-wk <thomas.maloney@workiva.com>
2020-07-06 00:36:58 -0500

    refactored FTodoServiceHandler and Persister

--- src/main/java/com/workiva/todo2/FTodoServiceHandler.java
+++ src/main/java/com/workiva/todo2/FTodoServiceHandler.java
@@ -1,6 +1,9 @@
 package com.workiva.todo2;
 
 import com.workiva.frugal.FContext;
+import com.workiva.messaging_sdk.exception.IamException;
+import com.workiva.messaging_sdk.iam.IamContext;
+
 import todo_transport.v1.FTodoService;
 import todo_transport.v1.Todo;
 import todo_transport.v1.TodoQueryParams;
@@ -8,77 +11,62 @@ import todo_transport.v1.WError;
 
 import org.apache.thrift.TException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-import java.util.UUID;
-import java.util.Map.Entry;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
- * A handler that handles all incoming requests to the server Currently stores
+ * A handler that handles all incoming requests to the server, currently stores
  * everything in memory for simplicity
  */
 public class FTodoServiceHandler implements FTodoService.Iface {
+    private static final Logger LOG = LoggerFactory.getLogger(FTodoServiceHandler.class);
 
     @Override
     public Todo createTodo(FContext ctx, Todo todo) throws TException, WError {
-        todo.id = newId();
-        InMemoryTodoPersister.getInstance().todoStore.put(todo.id, todo);
+        authorize(ctx);
+        InMemoryTodoPersister.createTodo(todo);
         return null;
     }
 
     @Override
     public void deleteTodo(FContext ctx, String todoID) throws TException, WError {
-        if (!InMemoryTodoPersister.getInstance().todoStore.containsKey(todoID)) {
-            throw new WError(1,"No such entry with ID: " + todoID);
-        }
-        InMemoryTodoPersister.getInstance().todoStore.remove(todoID);
+        authorize(ctx);
+        InMemoryTodoPersister.deleteTodo(todoID);
     }
 
     @Override
     public List<Todo> queryTodos(FContext ctx, TodoQueryParams params) throws TException, WError {
-        ArrayList<Todo> results = new ArrayList<>();
-        Set<Entry<String, Todo>> entrySet = InMemoryTodoPersister.getInstance().todoStore.entrySet();
-        Iterator<Entry<String,Todo>> it = entrySet.iterator();
-        while (it.hasNext()) {
-            Entry<String, Todo> pair = it.next();
-            if (params.includePublic && pair.getValue().isPublic) {
-                results.add(pair.getValue());
-                continue;
-            }
-            if (params.includePrivate && !pair.getValue().isPublic) {
-                results.add(pair.getValue());
-                continue;
-            }
-            if (params.includeComplete && pair.getValue().isCompleted) {
-                results.add(pair.getValue());
-                continue;
-            }
-            if (params.includeIncomplete && !pair.getValue().isCompleted) {
-                results.add(pair.getValue());
-                continue;
-            }
-        }
+        authorize(ctx);
+        List<Todo> results = InMemoryTodoPersister.queryTodos(params);
         return results;
     }
 
     @Override
     public Todo updateTodo(FContext ctx, Todo todo) throws TException, WError {
-        if (todo.id.isEmpty()) {
-            throw new WError(2, "Todo ID cannot be empty");
-        }
-        if (!InMemoryTodoPersister.getInstance().todoStore.containsKey(todo.id)) {
-            throw new WError(1,"No such entry with ID: " + todo.id);
-        }
-        InMemoryTodoPersister.getInstance().todoStore.put(todo.id, todo);
+        authorize(ctx);
+        InMemoryTodoPersister.updateTodo(todo);
         return null;
     }
 
-    private String newId() {
-        String uuid = UUID.randomUUID().toString();
-        return uuid.replace("-", "");
+    protected void authorize(FContext ctx) throws TException {
+        try {
+            IamContext iam = new IamContext(ctx);
+            if (!hasRoles(iam, new ArrayList<>())) {
+                TException err = new TException("Insufficient roles");
+                LOG.error("authorize error", err);
+                throw err;
+            }
+        } catch (IamException ie) {
+            TException err = new TException("Invalid IamContext", ie);
+            LOG.error("authorize error", err);
+            throw err;
+        }
     }
-    
+
+    private boolean hasRoles(IamContext ctx, List<String> roles) {
+        return true; //lmao implying I'm gonna do authorization
+    }
 }
--- src/main/java/com/workiva/todo2/InMemoryTodoPersister.java
+++ src/main/java/com/workiva/todo2/InMemoryTodoPersister.java
@@ -1,18 +1,82 @@
 package com.workiva.todo2;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.Map.Entry;
+
+import org.apache.thrift.TException;
 
 import todo_transport.v1.Todo;
+import todo_transport.v1.TodoQueryParams;
+import todo_transport.v1.WError;
 
 /**
  * Horrible antipattern singleton-esque persister
  */
 public class InMemoryTodoPersister {
     // leaving this public purely for demo
-    public HashMap<String, Todo> todoStore;
+    private HashMap<String, Todo> todoStore;
 
     private static InMemoryTodoPersister instance;
+
+    public static Todo createTodo(Todo todo) {
+        todo.id = newId();
+        getInstance().todoStore.put(todo.id, todo);
+        return todo;
+    }
+
+    public static void deleteTodo(String todoId) throws TException, WError {
+        if (!getInstance().todoStore.containsKey(todoId)) {
+            throw new WError(1,"No such entry with ID: " + todoId);
+        }
+        getInstance().todoStore.remove(todoId);
+    }
+
+    public static List<Todo> queryTodos(TodoQueryParams params) throws TException, WError {
+        ArrayList<Todo> results = new ArrayList<>();
+        Set<Entry<String, Todo>> entrySet = getInstance().todoStore.entrySet();
+        Iterator<Entry<String,Todo>> it = entrySet.iterator();
+        while (it.hasNext()) {
+            Entry<String, Todo> pair = it.next();
+            if (params.includePublic && pair.getValue().isPublic) {
+                results.add(pair.getValue());
+                continue;
+            }
+            if (params.includePrivate && !pair.getValue().isPublic) {
+                results.add(pair.getValue());
+                continue;
+            }
+            if (params.includeComplete && pair.getValue().isCompleted) {
+                results.add(pair.getValue());
+                continue;
+            }
+            if (params.includeIncomplete && !pair.getValue().isCompleted) {
+                results.add(pair.getValue());
+                continue;
+            }
+        }
+        return results;
+    }
+
+    public static Todo updateTodo(Todo todo) throws TException, WError {
+        if (todo.id.isEmpty()) {
+            throw new WError(2, "Todo ID cannot be empty");
+        }
+        if (!InMemoryTodoPersister.getInstance().todoStore.containsKey(todo.id)) {
+            throw new WError(1,"No such entry with ID: " + todo.id);
+        }
+        InMemoryTodoPersister.getInstance().todoStore.put(todo.id, todo);
+        return todo;
+    }
+
+    private static String newId() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid.replace("-", "");
+    }
 
     private InMemoryTodoPersister() {
         todoStore = new HashMap<>();
